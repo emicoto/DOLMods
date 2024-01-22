580affb0607dbb3aa1f11086f611beedc7ba3a2a
LyokoJeremie <lucheng989898@gmail.com>
2024-01-22 19:40:58 +0800

    type test code, use this to test types work well

---
+++ Simple Frameworks/types/test1.ts
@@ -0,0 +1,22 @@
+const a = new NamedNPC('', '', '', '');
+const b = new NamedNPC('', '', '', '');
+
+[].randompop();
+''.randompop();
+''.toCamelCase();
+[].has();
+
+window.roll(1, 1);
+
+window.SimplePatchPassage.patchPassage(a as any, '');
+window.SimplePatchPassage.patchedPassage;
+
+window.SelectCase
+window.check.好感('')
+window.cntv.变量('')
+
+setup.addNPCList.randompop();
+
+window.iMod.init('');
+
+setup.dolbus.randompop();
